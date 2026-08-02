43818ab5efffbbb384ec0bff5f6b94d3abc57279
sleevemcd <302004106+sleevemcd@users.noreply.github.com>
2026-08-02 22:51:15 +0000

    Add NAS deployment: docker-compose + install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Scoreboard HQ - NAS install / update
+# Run on the NAS (SSH or file station terminal).
+set -e
+
+REPO="https://github.com/sleevemcd/scoreboard-hq.git"
+DIR="/volume1/docker/scoreboard"
+
+if [ ! -d "$DIR/.git" ]; then
+  echo "== Cloning for the first time =="
+  git clone "$REPO" "$DIR"
+else
+  echo "== Pulling latest =="
+  cd "$DIR" && git pull
+fi
+
+echo "== Starting container =="
+cd "$DIR" && docker compose up -d --build
+
+echo "Done. Open http://<nas-ip>:1212/scoreboard.html"
